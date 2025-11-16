2e638eaa4364f4de7b577b734d064ddeba7ff1ca
joshkweitzel <joshuaweitzel75@gmail.com>
2025-11-16 12:49:44 -0500

    Motor speeds now alternate relative to the photo sensor

--- ActualCode/src/main.cpp
+++ ActualCode/src/main.cpp
@@ -1,13 +1,45 @@
 #include <Arduino.h>
 #include "AFMotor_R4.h"
 
-AF_DCMotor motor1(1);
+#define FAST 250
+#define SLOW 125
+AF_DCMotor motorL(3);
+AF_DCMotor motorR(1);
+volatile int photo;
+
+void checkLine();
 
 void setup() {
-  motor1.setSpeed(200);
-  motor1.run(FORWARD);
+  Serial.begin(9600);
+  motorL.setSpeed(0);
+  motorR.setSpeed(0);
+  motorL.run(FORWARD);
+  motorR.run(FORWARD);
 }
 
 void loop() {
+  checkLine();
+  if(photo == HIGH){
+    //Line detected
+    motorL.setSpeed(FAST);
+    motorR.setSpeed(SLOW);
+  } else {
+    //Line not detected
+    motorL.setSpeed(SLOW);
+    motorR.setSpeed(FAST);
+  }
   
+  delay(300);
+}
+
+void checkLine(){
+  //Converts analog input from the photo sensor into HIGH or LOW
+  if(analogRead(A0) >= 700){
+    photo = HIGH;
+    Serial.println("DETECTED");
+  } else {
+    photo = LOW;
+    Serial.println("NOT DETECTED");
+
+  }
 }
