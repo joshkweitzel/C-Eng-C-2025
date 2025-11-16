b3f5d97049585a9e3b8c8371d6c037f5b10f6095
joshkweitzel <joshuaweitzel75@gmail.com>
2025-11-16 14:55:58 -0500

    Adjusted speed

--- ActualCode/src/main.cpp
+++ ActualCode/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include "AFMotor_R4.h"
 
-#define FAST 100
-#define SLOW 50
+#define FAST 50
+#define SLOW 0
 AF_DCMotor motorL(3);
 AF_DCMotor motorR(1);
 volatile int photo;
